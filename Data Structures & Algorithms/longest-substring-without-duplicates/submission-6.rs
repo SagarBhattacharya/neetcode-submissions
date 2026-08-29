6da27b471557f3be2ef9cb76120c05cac059e1f8
Sagar Bhattacharya <92415621+SagarBhattacharya@users.noreply.github.com>
2026-08-29 20:38:29 +0530

    Add: longest-substring-without-duplicates - submission-6

---
+++ Data Structures & Algorithms/longest-substring-without-duplicates/submission-6.rs
@@ -0,0 +1,20 @@
+impl Solution {
+  pub fn length_of_longest_substring(s: String) -> i32 {
+    let mut longest = 0;
+    let (mut start, mut end) = (0, 0);
+    let bytes = s.as_bytes();
+
+    for i in 0..bytes.len() {
+      if bytes[start..end].contains(&bytes[i]) {
+        longest = longest.max(end - start);
+        while bytes[start] != bytes[i] {
+          start += 1;
+        }
+        start += 1;
+      }
+      end += 1;
+    }
+    longest = longest.max(end - start);
+    longest as i32
+  }
+}
