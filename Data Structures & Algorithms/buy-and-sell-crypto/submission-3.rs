032d4c3853f37d40d2d7606dd7acb0468f981822
Sagar Bhattacharya <92415621+SagarBhattacharya@users.noreply.github.com>
2026-08-29 18:56:06 +0530

    Add: buy-and-sell-crypto - submission-3

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-3.rs
@@ -0,0 +1,19 @@
+impl Solution {
+    pub fn max_profit(prices: Vec<i32>) -> i32 {
+        let mut buy_at = 0;
+        let mut sell_at = 1;
+        let mut max_profit = 0;
+
+        while sell_at < prices.len() {
+            let profit = prices[sell_at] - prices[buy_at];
+            if profit > 0 {
+                max_profit = max_profit.max(profit);
+            } else {
+                buy_at = sell_at;
+            }
+            sell_at += 1;
+        }
+
+        max_profit
+    }
+}
