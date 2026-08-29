408f26af36c5a9a17d97d3a6f2483a1e05c89439
Sagar Bhattacharya <92415621+SagarBhattacharya@users.noreply.github.com>
2026-08-29 22:47:21 +0530

    Add: longest-repeating-substring-with-replacement - submission-4

---
+++ Data Structures & Algorithms/longest-repeating-substring-with-replacement/submission-4.rs
@@ -0,0 +1,27 @@
+impl Solution {
+  pub fn character_replacement(s: String, k: i32) -> i32 {
+    use std::collections::HashMap;
+
+    let bytes = s.as_bytes();
+    let mut count = [0i32; 26];
+    let mut l = 0;
+    let mut result = 0;
+    let mut max = 0;
+
+    for r in 0..bytes.len() {
+      count[Self::idx(bytes[r])] += 1;
+      max = max.max(count[Self::idx(bytes[r])]);
+      while (r - l + 1) as i32 - max > k {
+        count[Self::idx(bytes[l])] -= 1;
+        l += 1;
+        max = *count.iter().max().unwrap();
+      }
+      result = result.max(r - l + 1);
+    }
+    result as i32
+  }
+
+  fn idx(c: u8) -> usize {
+    (c - b'A') as usize
+  }
+}
