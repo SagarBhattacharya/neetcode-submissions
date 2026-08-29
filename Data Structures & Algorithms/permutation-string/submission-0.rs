a544ffd44dd47b3802338de6b918eef59cdce620
Sagar Bhattacharya <92415621+SagarBhattacharya@users.noreply.github.com>
2026-08-29 23:44:48 +0530

    Add: permutation-string - submission-0

---
+++ Data Structures & Algorithms/permutation-string/submission-0.rs
@@ -0,0 +1,40 @@
+impl Solution {
+  pub fn check_inclusion(s1: String, s2: String) -> bool {
+    let b1 = s1.as_bytes();
+    let b2 = s2.as_bytes();
+
+    if b1.len() > b2.len() {
+      return false;
+    }
+
+    let mut need = [0i32; 26];
+    let mut window = [0i32; 26];
+
+    b1.iter()
+      .for_each(|&c| need[Self::idx(c)] += 1);
+    b2.iter().take(b1.len())
+      .for_each(|&c| window[Self::idx(c)] += 1);
+
+    if window == need {
+      return true;
+    }
+
+    for r in b1.len()..b2.len() {
+      let entering = Self::idx(b2[r]);
+      let leaving = Self::idx(b2[r - b1.len()]);
+
+      window[entering] += 1;
+      window[leaving] -= 1;
+
+      if window == need {
+        return true;
+      }
+    }
+
+    false
+  }
+
+  fn idx(c: u8) -> usize {
+    (c - b'a') as usize
+  }
+}
