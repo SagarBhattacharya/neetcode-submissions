2f2f9e41bd14b9bdfbf43c75a124c3b00cddb9c8
Sagar Bhattacharya <92415621+SagarBhattacharya@users.noreply.github.com>
2026-08-30 15:39:10 +0530

    Add: evaluate-reverse-polish-notation - submission-0

---
+++ Data Structures & Algorithms/evaluate-reverse-polish-notation/submission-0.rs
@@ -0,0 +1,38 @@
+impl Solution {
+  pub fn eval_rpn(tokens: Vec<String>) -> i32 {
+    let mut stack = vec![];
+    for token in tokens {
+      match token.as_str() {
+        "+" => {
+          let b = stack.pop().unwrap();
+          let a = stack.pop().unwrap();
+          stack.push(a + b);
+        },
+        "-" => {
+          let b = stack.pop().unwrap();
+          let a = stack.pop().unwrap();
+          stack.push(a - b);
+        },
+        "*" => {
+          let b = stack.pop().unwrap();
+          let a = stack.pop().unwrap();
+          stack.push(a * b);
+        },
+        "/" => {
+          let b = stack.pop().unwrap();
+          let a = stack.pop().unwrap();
+          stack.push(a / b);
+        },
+        num => {
+          if let Ok(num) = num.parse::<i32>() {
+            stack.push(num);
+          } else {
+            // error case
+          }
+        }
+      }
+    }
+
+    stack[0]
+  }
+}
