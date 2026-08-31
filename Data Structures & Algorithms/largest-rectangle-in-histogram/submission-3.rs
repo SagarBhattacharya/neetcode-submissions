b9f4cd6e4ab5aa5dc338c5bdd2bcbd3f52fb48ec
Sagar Bhattacharya <92415621+SagarBhattacharya@users.noreply.github.com>
2026-08-31 14:03:41 +0530

    Add: largest-rectangle-in-histogram - submission-3

---
+++ Data Structures & Algorithms/largest-rectangle-in-histogram/submission-3.rs
@@ -0,0 +1,33 @@
+impl Solution {
+    pub fn largest_rectangle_area(heights: Vec<i32>) -> i32 {
+        let mut stack: Vec<usize> = Vec::new();
+        let mut max_area = 0;
+
+        for i in 0..=heights.len() {
+            let current = if i == heights.len() {
+                0
+            } else {
+                heights[i]
+            };
+
+            while let Some(&top) = stack.last() {
+                if heights[top] <= current {
+                    break;
+                }
+
+                stack.pop();
+                let height = heights[top];
+                let left = stack.last()
+                    .map_or(0, |&idx| idx + 1);
+                let width = i - left;
+                max_area = max_area.max(height * width as i32);
+            }
+
+            if i < heights.len() {
+                stack.push(i);
+            }
+        }
+
+        max_area
+    }
+}
